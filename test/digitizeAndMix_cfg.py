0ade5ba492d159ecca9721adbe7bb611a069be64
Pedro Silva <psilva@cern.ch>
2014-12-01 10:27:24 +0100

    updating

--- test/digitizeAndMix_cfg.py
+++ test/digitizeAndMix_cfg.py
@@ -117,7 +117,8 @@ process = cust_2023HGCalMuon(process)
 
 #custom digitization
 from SimCalorimetry.HGCSimProducers.customHGCdigitizer_cfi import customHGCdigitizer
-customHGCdigitizer(process,'femodel',True)
+#customHGCdigitizer(process=process,vesion="femodel",debug=True)
+customHGCdigitizer(process=process,version="simple0",debug=True)
 
 print 'Will digitize with the following parameters'
 print process.source.fileNames
